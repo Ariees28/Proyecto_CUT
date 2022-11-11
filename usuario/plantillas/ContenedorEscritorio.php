ab051a28211c450bbf24d6df74f0b6706cbfe988
Jorge Rodriguez <sirius-black24@hotmail.com>
2022-11-11 13:35:17 -0600

    prueba
    prueba deploy automatico

--- usuario/plantillas/ContenedorEscritorio.php
+++ usuario/plantillas/ContenedorEscritorio.php
@@ -6,5 +6,4 @@
 <div class="app-main__inner" id="ContenedorPrincipal">
     <h1>HOLA MUNDO! Este será nuestrto escritorio principal</h1>
     <?php echo "<h1>BIENVENIDO ".$_SESSION["nombre"]."</h1>" ?>
-    <h1>AH QUE LA CHINGÁ</h1>
 </div>
